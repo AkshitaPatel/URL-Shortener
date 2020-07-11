7a77fe8190248f82b5a6e10f7b5df10b12c2fddc
Akshita Patel <akshitapatel02@gmail.com>
2020-07-11 19:20:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrls')
 const app = express()
 
-mongoose.connect('mongodb+srv://akshita123:akshita123@akshita-yjs1o.mongodb.net/UrlShortener?retryWrites=true&w=majority',{
+mongoose.connect(DATABASE_URL,{
   useNewUrlParser: true, useUnifiedTopology: true
 })
 
